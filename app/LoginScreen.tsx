16098726213606e6c71ee04d8a8a05dcf21e8f10
Ayman Rahman <ayman2002rahman@gmail.com>
2025-03-22 10:24:48 -0500

    Login screen pretty much done

--- app/LoginScreen.tsx
+++ app/LoginScreen.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { Alert, StyleSheet, TouchableOpacity, View, Text, Button, Touchable, TextInput } from 'react-native';
+import { Alert, StyleSheet, TouchableOpacity, View, Text, Button, Touchable, TextInput, Image } from 'react-native';
+import GradientText from './components/GradientText';
 import { supabase } from './lib/supabase';
 // import { Input, InputField } from '../../components/ui/input';
 // import { Button, ButtonText } from '../../components/ui/button';
 import { useRouter } from 'expo-router';
 
+
 export default function LoginScreen() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [loading, setLoading] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
+
 	const router = useRouter();
 
 	async function signInWithEmail() {
@@ -27,6 +31,7 @@ export default function LoginScreen() {
 			});
 
 		setLoading(false);
+		router.navigate('./tabs/HomeScreen');
 	}
 
 	async function signUpWithEmail() {
@@ -46,25 +51,54 @@ export default function LoginScreen() {
 
 	return (
 		<View style={styles.container}>
-			<View style={styles.fields}>
-				<TextInput
-					style={styles.input}
-					value={email}
-					onChangeText={setEmail}
-					placeholder="Enter email"
-					placeholderTextColor="#999"
-				/>
-				<TextInput
-					style={styles.input}
-					value={email}
-					onChangeText={setEmail}
-					placeholder="Enter password"
-					placeholderTextColor="#999"
+			<View style={{ position: 'absolute', top: 100 }}>
+				<Image
+					source={{ uri: 'https://vwqokxwilhvpiybjgsbt.supabase.co/storage/v1/object/public/images//logo.png' }}
+					style={{ width: 186, height: 186 }}
+					resizeMode="contain" // or 'cover', 'stretch', etc.
 				/>
 			</View>
-			<TouchableOpacity style={styles.button} onPress={() => router.navigate('./tabs/HomeScreen')}>
-				<Text style={styles.buttonText}>Start Listening</Text>
-			</TouchableOpacity>
+			<View style={{ position: 'relative', top: -70, left: -12 }}>
+				<Text style={styles.phrase}>
+					Get into the{'\n'}rhythm of{' '}
+				</Text>
+				<GradientText text="learning" />
+			</View>
+			<View style={styles.bottom}>
+				<View style={styles.fields}>
+					<TextInput
+						style={styles.input}
+						value={email}
+						onChangeText={setEmail}
+						placeholder="Enter email"
+						placeholderTextColor="#999"
+					/>
+					<View style={styles.passwordContainer}>
+					<TextInput
+						style={styles.input}
+						value={password}
+						onChangeText={setPassword}
+						placeholder="Password"
+						placeholderTextColor="#999"
+						secureTextEntry={!showPassword}
+					/>
+					<TouchableOpacity
+						style={styles.eyeIcon}
+						onPress={() => setShowPassword(!showPassword)}
+					>
+						<Text style={{ color: '#999' }}>
+						{showPassword ? '🙈' : '👁️'}
+						</Text>
+					</TouchableOpacity>
+					</View>
+					<TouchableOpacity>
+						<Text style={styles.forgot}>Forgot Password</Text>
+					</TouchableOpacity>
+				</View>
+				<TouchableOpacity style={styles.button} onPress={() => signInWithEmail()}>
+					<Text style={styles.buttonText}>Start Listening</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 }
@@ -84,6 +118,16 @@ const styles = StyleSheet.create({
 	},
 	mt20: {
 		marginTop: 20,
+	},
+	bottom: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 100,
+		position: 'absolute',
+		bottom: 100,
+		left: 0,
+		right: 0,
 	},
 	button: {
 		display: 'flex',
@@ -119,8 +163,45 @@ const styles = StyleSheet.create({
 		borderWidth: 2,
 		borderColor: 'rgba(132, 42, 247, 0.7)',
 		opacity: 0.7,
+		color: '#FFF',
 	},
 	fields: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 15,
+	},
+	forgot: {
+		overflow: 'hidden',
+		color: 'rgba(255, 255, 255, 0.5)',
+		fontFamily: 'Roboto',
+		fontSize: 14,
+		fontStyle: 'normal',       // optional (default)
+		fontWeight: '400',
+		lineHeight: 21,
+	},
+	phrase: {
+		width: 283,
+		flexShrink: 0,
+		color: 'rgba(142, 141, 141, 0.81)',
+		fontFamily: 'Roboto',
+		fontSize: 48,
+		fontStyle: 'normal',   // optional (default)
+		fontWeight: '600',     // must be a string
+		lineHeight: 48,
 
-	}
+	},
+	learning: {
+
+	},
+	passwordContainer: {
+		position: 'relative',
+		width: 313,
+	  },
+	  
+	  eyeIcon: {
+		position: 'absolute',
+		right: 12,
+		top: 15,
+	  },
 });
